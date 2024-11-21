041094c6082571d916e8de789740d7939bbcab30
Bermet <iskenderova.bermet19@gmail.com>
2024-11-21 19:02:55 +0600

    Promote Employee button changes the Position

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,17 +1,17 @@
 import  { useState } from 'react'
 const Component = () => {
-    const[message, setMessage] = useState("");
+    const [position, setPosition] = useState("Software Developer");
 
     return (
         <div>
             <h1> Name: Bermet Iskenderova</h1>
-            <p> Position: Developer </p>
+            <p> Position: {position} </p>
             <p>Company: development.kg</p>
             <p> Age: 19 Years old </p>
             <p> Location: Bishkek city /Kyrgyzstan </p>
             <p>Email: iskenderova.bermet19@gmail.com</p>
-            {message &&  <p>{message}</p>}
-            <button onClick={()=> setMessage ("Employee Information Displayed!" )}>Promote Employee</button>
+            <button 
+            onClick ={()=> setPosition("Senior Software Developer ")}>Promote Employee</button>
         </div>
     );
 };
