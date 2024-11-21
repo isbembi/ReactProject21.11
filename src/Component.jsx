6aa497ac9dfbd138245b7ffeecea6ccfbacb218b
Bermet <iskenderova.bermet19@gmail.com>
2024-11-21 18:26:26 +0600

    Component added

---
+++ src/Component.jsx
@@ -0,0 +1,14 @@
+const Component = () => {
+    return (
+        <div>
+            <h1> Name: Bermet Iskenderova</h1>
+            <p> Position: Developer </p>
+            <p>Company: development.kg</p>
+            <p> Age: 19 Years old </p>
+            <p> Location: Bishkek city /Kyrgyzstan </p>
+            <p>Email: iskenderova.bermet19@gmail.com</p>
+        </div>
+    );
+};
+
+export default Component;
